f6c68aecaf4ff1ca3d011b50787f8dee768747a0
King <kingdavidmartins@users.noreply.github.com>
2018-02-08 16:15:12 -0500

    WIp

--- internal/workqueue/minions.go
+++ internal/workqueue/minions.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"log"
+	"container/list"
 )
 
 func fetch(url string) (string, error) {
@@ -26,7 +27,8 @@ func parse(text string) ([]string, error) {
 	return outgoing_urls, nil
 }
 
-func process(url string, buff []string) {
+func process(url string, buff *list.List) {
+	log.Printf("processing %s", url)
 	text, err := fetch(url)
 	if err != nil {
 		log.Fatalf("SH!T GOT REAL ~ Failed to parse url %s\n", url)
@@ -38,6 +40,47 @@ func process(url string, buff []string) {
 	}
 
 	for _, u := range outgoing_urls {
-		buff = append(buff, u)
+		buff.PushBack(u)
+	}
+}
+
+func pop(lst *list.List) string {
+	head := lst.Front()
+
+	lst.Remove(head)
+	return head.Value.(string)
+}
+
+func worker(id int, queue chan string, store map[string]string) {
+	internal_buffer := list.New()
+
+	for url := range queue {
+		if store[url] != "" {
+			continue
+		}
+
+		log.Printf("worker %d received %s from queue", id, url)
+		process(url, internal_buffer)
+
+		for {
+			if internal_buffer.Len() == 0 {
+				break
+			}
+
+			next_url := pop(internal_buffer)
+			log.Printf("worker %d grabbed %s from internal buffer", id, next_url)
+			select {
+			case queue <- next_url: 
+			  // do nothing
+			default: 
+				process(next_url, internal_buffer)
+			}
+		}
+	}
+}
+
+func SpinUpWorkers(num int, queue chan string, store map[string]string) {
+	for id := 0; id < num; id++ {
+		go worker(id, queue, store)
 	}
 }
