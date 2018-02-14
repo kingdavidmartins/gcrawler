374ee50eca68649216706a685699966d6dde138e
King <kingdavidmartins@users.noreply.github.com>
2018-02-14 11:28:20 -0500

    add func process() && func pop()

--- internal/workqueue/minions.go
+++ internal/workqueue/minions.go
@@ -1,9 +1,12 @@
 package workqueue
 
 import (
+	"container/list"
 	"errors"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"sync"
 )
 
 func fetch(url string) (string, error) {
@@ -22,5 +25,34 @@ func fetch(url string) (string, error) {
 
 func parse(text string) ([]string, error) {
 	outgoingUrls := make([]string, 0)
-	return outgoing_urls, nil
+	return outgoingUrls, nil
+}
+
+func process(url string, buff *list.List, store map[string]string, storeMutex *sync.RWMutex) {
+	log.Printf("processing %s", url)
+	text, err := fetch(url)
+	if err != nil {
+		log.Fatalf("SH!T GOT REAL ~ Failed to parse url %s\n", url)
+	}
+
+	outgoingUrls, err := parse(text)
+	if err != nil {
+		log.Fatalf("SH!T GOT REAL ~ Failed to parse body from %s\n", url)
+	}
+
+	// push into store
+	storeMutex.Lock()
+	store[url] = url
+	storeMutex.Unlock()
+
+	for _, u := range outgoingUrls {
+		buff.PushBack(u)
+	}
+}
+
+func pop(lst *list.List) string {
+	head := lst.Front()
+
+	lst.Remove(head)
+	return head.Value.(string)
 }
