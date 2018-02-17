b5efc8123a26ff1d340f0bdc340ea1ea322d365f
King <kingdavidmartins@users.noreply.github.com>
2018-02-16 22:12:10 -0500

    add spinUpWorkers & workers

--- internal/workqueue/minions.go
+++ internal/workqueue/minions.go
@@ -56,3 +56,40 @@ func pop(lst *list.List) string {
 	lst.Remove(head)
 	return head.Value.(string)
 }
+
+func worker(id int, queue chan string, store map[string]string, store_mutex *sync.RWMutex) {
+	internal_buffer := list.New()
+
+	for url := range queue {
+		store_mutex.Lock()
+		if store[url] != "" {
+			store_mutex.Unlock()
+			continue
+		}
+		store_mutex.Unlock()
+
+		log.Printf("worker %d received %s from queue", id, url)
+		process(url, internal_buffer, store, store_mutex)
+
+		for {
+			if internal_buffer.Len() == 0 {
+				break
+			}
+
+			next_url := pop(internal_buffer)
+			log.Printf("worker %d grabbed %s from internal buffer", id, next_url)
+			select {
+			case queue <- next_url:
+				// do nothing
+			default:
+				process(next_url, internal_buffer, store, store_mutex)
+			}
+		}
+	}
+}
+
+func SpinUpWorkers(num int, queue chan string, store map[string]string, store_mutex *sync.RWMutex) {
+	for id := 0; id < num; id++ {
+		go worker(id, queue, store, store_mutex)
+	}
+}
